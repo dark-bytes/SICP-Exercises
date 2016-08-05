c089a2fdb72dede58fb9d3d94d481b4fa8ffee30
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-05 15:45:14 +0530

    Exercise 1.12
    Done exercise 1.12

---
+++ Chapter 1/ex 1.12.rkt
@@ -0,0 +1,23 @@
+#lang scheme
+
+;pascal triangles
+;tell us the row and column of triangle
+;starts from 0th row
+
+(define (fact n)(
+                  if (<= n 1)
+                       1
+                       ( * n (fact (- n 1)))))
+                     
+
+
+
+(define ( pasc  r c)(
+          if (<= c (+ r 1) )
+          (/ (fact r) (* (fact c) (fact (- r c))))
+          0
+      )
+)
+
+
+(pasc 2 3)
