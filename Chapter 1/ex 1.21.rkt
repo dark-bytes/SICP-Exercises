ea160e51dcaf078a9d4e2f3ff9ea16587ac2f05e
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-08 13:55:13 +0530

    Exercise 1.21

---
+++ Chapter 1/ex 1.21.rkt
@@ -0,0 +1,14 @@
+#lang scheme
+
+(define (smallest-divisor n start)
+  ( cond ((> (* start start) n) n)
+         ((divide? start n) start)
+         (else (smallest-divisor n (+ start 1))))
+   )
+
+(define (divide? a b)
+  (= (remainder b a) 0 ))
+
+( smallest-divisor 199 2)
+(smallest-divisor 1999 2)
+(smallest-divisor 19999 2)
