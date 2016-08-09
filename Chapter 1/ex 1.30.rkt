696ef27aff22b2aacf68ca07bfdaa9f6505053f1
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-09 15:27:35 +0530

    Exercise 1.29-1.30

--- Chapter 1/ex 1.30.rkt
+++ Chapter 1/ex 1.30.rkt
@@ -1,10 +1,13 @@
 #lang scheme
 
 (define (sum term a next b) 
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next a) next b)))
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result ( term a)))
+        )
+    )
+  (iter a 0)
 )
 
 (define (round-to-next-even x) 
