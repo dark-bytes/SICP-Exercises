4a80de4ac106469ec758ba1494d18c1b3d0e74ee
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-10 11:51:01 +0530

    Exercise 1.35

---
+++ Chapter 1/ex 1.35.rkt
@@ -0,0 +1,21 @@
+#lang scheme
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs ( - v1 v2)) tolerance ))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess)
+ )
+
+
+(fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1000.0)
