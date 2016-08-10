b8d77dc2d2c715aca9d7584e3cfd8ebd5e1bcfbe
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-10 13:40:48 +0530

    exercise 1.39

---
+++ Chapter 1/ex 1.39.rkt
@@ -0,0 +1,23 @@
+#lang scheme
+
+(define tolerance 0.00001)
+(define sum 2)
+(define (inc x) (+ x 1))
+(define (next d)
+  (+ d 2)
+  )
+(define (square x) (* x x))
+
+
+(define (cont-frac x d k)
+  ( cond (( = k 0) 0)
+    ( else (/ (square x) (- d (cont-frac x (next d) (- k 1))) ) 
+  )
+ )
+)
+
+(define (tan-cf x k)
+  (/ x cont-frac x 1.0 k)
+)
+
+(tan-cf 1.0 5)
