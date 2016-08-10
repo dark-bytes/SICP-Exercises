3009c2f910494ee9ede52f1601ac8d3f4522f107
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-10 12:57:24 +0530

    Exercise 1.37

---
+++ Chapter 1/ex 1.37.rkt
@@ -0,0 +1,15 @@
+#lang scheme
+
+(define tolerance 0.00001)
+(define (inc x) (+ x 1))
+
+(define (cont-frac n d k)
+  ( cond (( = k 0) 0)
+    ( else (/ n (+ d (cont-frac n d (- k 1))) ) 
+   ))
+)
+
+
+
+(cont-frac  1.0 1.0 11)
+
