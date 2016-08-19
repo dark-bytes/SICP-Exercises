d472c42339209aa576c431448668ddd09c09562e
sud-curiosum <sudhanshusingh3@hotmail.com>
2016-08-19 13:33:32 +0530

    Exercise 2.27

---
+++ Chapter 2/ex 2.27.rkt
@@ -0,0 +1,21 @@
+#lang scheme
+
+(define (deep-reverse list1)
+  ( cond ((null? list1) null)
+         ((pair? (car list1))
+          (append
+           (deep-reverse (cdr list1))
+           (list (deep-reverse (car list1))))
+         
+         )
+         (else
+          (append
+           (deep-reverse (cdr list1))
+           (list (car list1))))
+         ))
+
+(define x (list (list 1 2) (list 3 4)))
+;(display x)
+(cdr x)
+;(reverse (list 1 2 3 4 5))
+(deep-reverse x)
